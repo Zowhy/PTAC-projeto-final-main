b83236cbd158a22929a1e055f431bf62c4c393fd
Zowhy <dtrovato892@gmail.com>
2023-12-08 17:02:37 -0400

    Configurações do Home

---
+++ PTAC-projeto-final-main/src/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home(){
+    return(
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+  </div>
+</nav>
+     );
+}
