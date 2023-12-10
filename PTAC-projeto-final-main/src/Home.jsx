98827575720c87f8a67f015dddd2fc3d83a27f61
Zowhy <dtrovato892@gmail.com>
2023-12-10 17:12:09 -0300

    Home
    representa a estrutura de uma página react que exibe uma lista de vídeos favoritos

--- PTAC-projeto-final-main/src/Home.jsx
+++ PTAC-projeto-final-main/src/Home.jsx
@@ -1,9 +1,27 @@
-export default function Home(){
-    return(
-<nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-  </div>
-</nav>
-     );
+import React from 'react';
+import Video from './Video';
+
+import { videos } from "../videos";
+
+import "./Home.css"
+import { Aside } from './Aside';
+
+const Home = () => {
+
+  return (
+    <div className="container">
+      <Aside />
+
+      <div className='home'>
+        <h2>Meus Vídeos Favoritos</h2>
+        <div className='videos'>
+          {videos.map(video => (
+            <Video key={video.id} title={video.title} url={video.url} id={video.id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default Home;
