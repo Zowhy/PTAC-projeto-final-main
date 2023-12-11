92d93d5dd743673bba4ee21461b8ef52f4f87bc4
Zowhy <dtrovato892@gmail.com>
2023-12-11 09:53:34 -0300

    Encontrar o Id
    Coloquei uma função find onde ele vai localizar o video no seu id

--- PTAC-projeto-final-main/src/VideoDetails.jsx
+++ PTAC-projeto-final-main/src/VideoDetails.jsx
@@ -3,6 +3,10 @@ import { videos } from "../videos";
 import { Aside } from "./Aside";
 
 import "./VideoDetails.css";
-
 export function VideoDetails() {
+
   const { id } = useParams();
+
+  const videoWithSameId = videos.find(video => String(video.id) === id);
+
+}
