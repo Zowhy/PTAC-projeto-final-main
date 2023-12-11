ef8eebb74d29d1e6b480005e93d221f642ec8339
Zowhy <dtrovato892@gmail.com>
2023-12-11 10:00:15 -0300

    Rotas
    Ele vai definir varias rotas e vai associar cada componente especifico

--- PTAC-projeto-final-main/src/main.jsx
+++ PTAC-projeto-final-main/src/main.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Home'
+import { Destaque } from './Destaque'
+import NovoVideo from './NovoVideo'
+import { Detalhe } from './Detalhe'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <BrowserRouter>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/Detalhe/:id" element={<Detalhe />} />
+        <Route path="/destaque" element={<Destaque />} />
+        <Route path="/novo-video" element={<NovoVideo />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
