7291846d17b3b614ff8efbd442f0cad467b30820
Zowhy <dtrovato892@gmail.com>
2023-12-11 08:59:14 -0300

    Renderização
    Vai renderizar o que esta na tela, e o link vai levar para a pagina detalhe do video quando você clicar

--- PTAC-projeto-final-main/src/componentes/Card.jsx
+++ PTAC-projeto-final-main/src/componentes/Card.jsx
@@ -5,5 +5,16 @@ export function Card({video}) {
 
 const videoPegado = video.url.slice(17)
 
-
-}
+  return (
+<Link to={`/detalhe/${video.id}`} >
+  <h1> {video.name} </h1>
+    <iframe 
+    width="300" 
+    height="200"
+    src={`https://www.youtube.com/embed/${videoPegado}`} 
+    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+    </iframe> 
+    
+    </Link>
+  )
+ };
