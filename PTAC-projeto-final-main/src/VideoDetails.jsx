7784d4d91398612861fd33ae27e762e3e58b307f
Zowhy <dtrovato892@gmail.com>
2023-12-11 09:55:41 -0300

    Renderização
    Vai renderizar com a estrutura do Html e utiliza as propiedades do video para encontrar e exibir o tiulo

--- PTAC-projeto-final-main/src/VideoDetails.jsx
+++ PTAC-projeto-final-main/src/VideoDetails.jsx
@@ -3,10 +3,27 @@ import { videos } from "../videos";
 import { Aside } from "./Aside";
 
 import "./VideoDetails.css";
-export function VideoDetails() {
 
+export function VideoDetails() {
   const { id } = useParams();
 
   const videoWithSameId = videos.find(video => String(video.id) === id);
 
+  return (
+    <div className="container">
+      <Aside />
+      
+      <div className="container-video">
+        <div className="card">
+          <h2>{videoWithSameId.title}</h2>
+
+          <div className="details">
+            <iframe src={videoWithSameId.url} width={500} height={300}></iframe>
+
+            <span>{videoWithSameId.description}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
