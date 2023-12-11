5b1240000df48516cf33b6c31c0b10c6b290fe27
Zowhy <dtrovato892@gmail.com>
2023-12-10 23:14:06 -0300

    Useparams
    ele meio que extrai o parâmetro id da url

--- PTAC-projeto-final-main/src/Detalhe.jsx
+++ PTAC-projeto-final-main/src/Detalhe.jsx
@@ -2,3 +2,6 @@ import React from 'react';
 import "./Home.css"
 import { Aside } from './Aside';
 import { useParams } from 'react-router-dom';
+
+export function Detalhe ()  {
+  const {id} = useParams ()
