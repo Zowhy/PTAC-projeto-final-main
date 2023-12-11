b66ce7c3ed4f3b3b8601f6b9b27468abd81db5db
Zowhy <dtrovato892@gmail.com>
2023-12-10 23:21:39 -0300

    Renderização
    Vai renderizar as informações na tela

--- PTAC-projeto-final-main/src/Detalhe.jsx
+++ PTAC-projeto-final-main/src/Detalhe.jsx
@@ -9,3 +9,18 @@ export function Detalhe ()  {
   const videoSaves = JSON.parse(localStorage.getItem("Lista")) || [];const filterVideos = videoSaves.find((object) => object.id == id) || null
   const filterVideos = videoSaves.find((object) => object.id == id) || null
   console.log (videoSaves)
+
+  return (
+    <div className="container">
+      <Aside />
+
+      <div className='home'>
+        <h2>Detalhe</h2>
+        <div className='videos'>
+        {filterVideos.name }  {filterVideos.canal } {filterVideos.data }  {filterVideos.local }  {filterVideos.letra }  
+          
+        </div>
+      </div>
+    </div>
+  );
+}
